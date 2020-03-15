a447edb12fecd31a8af4e21b04d1d863533255fb
sovaz1997 <sovaz1997@yandex.ru>
2020-03-15 16:31:16 +0300

    feat: clone check working

--- prog.py
+++ prog.py
@@ -1,6 +1,8 @@
 import git
 import os
 
+from bs4 import BeautifulSoup
+
 
 def getPercent(value):
   return f'{value * 100}%'
@@ -12,22 +14,43 @@ def get_jaccard_sim(text1, text2):
 
     return float(len(c)) / (len(a) + len(b) - len(c))
 
+
+def parseScores(path):
+  pageText = open(path,'r').read()
+  page = BeautifulSoup(pageText, 'html.parser')
+  
+  users = set()
+
+  for user in page.find_all('tr'):
+    if user.has_attr('data-row-key'):
+      users.add(user.get('data-row-key'))
+
+  return list(users)
+  
+  
+
 class UserTask:
   def __init__(self, userName, taskName, localPath):
     self.userName = userName
     self.taskName = taskName
     self.localPath = localPath
 
-    self._cloneProject()
+    self.success = self._cloneProject()
   
   def _cloneProject(self):
     self.path = os.path.join(self.localPath, self.userName)
 
     try:
       os.makedirs(self.path)
-      git.Git(self.path).clone(f'https://github.com/{self.userName}/{self.taskName}.git')
     except OSError:
       print ("Creation of the directory %s failed" % self.path)
+
+    if not os.listdir(self.path):
+      try:
+        git.Git(self.path).clone(f'https://github.com/{self.userName}/{self.taskName}.git')
+      except git.exc.GitError:
+        return False
+    return True
 
   def getText(self, path):
     with open (os.path.join(self.path, self.taskName, path), "r") as f:
@@ -43,8 +66,15 @@ class UserList:
     self._createUserTasks(users)
 
   def _createUserTasks(self, users):
+    i = 0
     for user in users:
-      self.usersTasks[user] = UserTask(user, self.taskName, self.localPath)
+      task = UserTask(user, self.taskName, self.localPath)
+      
+      if task.success:
+        self.usersTasks[user] = task
+      
+        print(f'Downloaded: {i + 1}/{len(users)}')
+        i += 1
   
   def compare(self, userNameA, userNameB, path):
     userA = self.usersTasks[userNameA]
@@ -71,7 +101,7 @@ class UserList:
       for userA in self.usersTasks:
         for userB in self.usersTasks:
           if userA != userB:
-            res = self.cloneCheck(taskPath, userA, userB, 0.9)
+            res = self.cloneCheck(taskPath, userA, userB, 0.95)
             if res:
               print(self.createResultRow(taskPath, userA, userB, res))
 
@@ -84,9 +114,9 @@ class UserList:
         print(task.getText(path))
 
 if __name__ == "__main__":
-  users = ['sovaz1997', 'hallovarvara', 'inq666', 'torchik-slava', 'alexeikravchuk']
+  users = parseScores(os.path.join('.', 'scores', '1.html'))
   chechPaths = [
-    os.path.join('src', 'carbon-dating.js')
+    os.path.join('src', 'vigenere-cipher.js')
   ]
 
   userList = UserList(users, 'basic-js', os.path.join('.', 'data'), chechPaths)
